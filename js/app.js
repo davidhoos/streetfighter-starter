e01cd8991b8f836da431d2856ce802955d018916
davidhoos <david.c.hoos@gmail.com>
2015-01-31 10:53:38 -0800

    Finished projects

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-    	playHadouken();
-    	$('.ryu-ready').hide();
-    	$('.ryu-throwing').hide();
-    	$('.hadouken').show().animate(
-  			{'left': '300px'},
-  			500,
-  			function() {
-    			$(this).hide();
-    			$(this).css('left', '-212px');
-			  }
-			);
-    	// animate hadouken to the right of the screen
-  	})
-	.mouseup(function() {
-   		$('.ryu-throwing').hide();
-   		$('.ryu-ready').show();
-	})
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
 });
 
 function playHadouken () {
@@ -32,4 +32,21 @@ function playHadouken () {
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
+  $(document).keydown(function(e) {
+          if (e.keyCode == '88') {
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.ryu-cool').show();
+          }
+        })
+            .keyup(function(e) {
+          if (e.keyCode == '88') {
+            $('.ryu-still').show();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.ryu-cool').hide();
+          }
+        });
 
